a8e0b6dc92098eff4c454f59bd5d965ff5287df2
Zhan <i522331@kaelmbp.dhcp.pvgl.sap.corp>
2019-11-21 15:12:21 +0800

    use both environment.yml and .venv to activate conda env

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ conda_auto_activate
 Automatically activate a conda environment when entering folders/project.
 
 conda_auto_activate automatically activates a conda environment when
-enterring a folder that contains a `.condaauto` file.  The first line in the
-`.condaauto` file is the name of the conda environment.
+enterring a folder that contains a `environment.yml` or `.venv` file. 
+The `environment.yml` can exported from a existed conda env and we 
+use it to create a new conda env. The first line in the `.venv` file
+is the name of the conda env.
 
-To make this work you have to source `conda_auto_activate.sh` from your
-`~/.bashrc`.
+To make this work you have to source conda_auto_activate.sh from your
+~/.bashrc or ~/.zshrc.
+
+
 
 ```
 source ~/path_to/conda_auto_activate.sh
 ```
-
-conda_auto_activate is inspired by [virtualenv-auto-activate.sh](https://gist.github.com/garyjohnson/394c58e22a2adfa103e2).
--- conda_auto_activate.sh
+++ conda_auto_activate.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
 # conda_auto_activate automatically activates a conda environment when
-# enterring a folder that contains a .condaauto file. The first line in the
-# .condaauto file is the name of the conda environment.
+# enterring a folder that contains a `environment.yml` or `.venv` file. 
+# The `environment.yml` can exported from a existed conda env and we 
+# use it to create a new conda env. The first line in the `.venv` file
+# is the name of the conda env.
 #
 # To make this work you have to source conda_auto_activate.sh from your
-# ~/.bashrc.
+# ~/.bashrc or ~/.zshrc.
 
 function _conda_auto_activate() {
-  if [ -e ".condaauto" ]; then
-    # echo ".condaauto file found"
-    ENV=$(head -n 1 .condaauto)
-
-    # Check to see if already activated to avoid redundant activating
-    if [[ $PATH == *"$ENV"* ]]; then
-      echo "Conda env '$ENV' already activated."
-    else
+  if [ -e "environment.yml" ]; then
+    # echo "environment.yml file found"
+    ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
+    # Check if you are already in the environment
+    if [[ $PATH != *$ENV* ]]; then
       source activate $ENV
+      # Check if the environment exists
+      if [ $? -eq 0 ]; then
+        :
+      else
+        # Create the environment and activate
+        echo "Conda env '$ENV' doesn't exist."
+        conda env create -q
+        source activate $ENV
+      fi
+    fi
+  elif [ -e ".venv" ]; then
+    # echo ".venv file found"
+    ENV=$(head -n 1 .venv)
+    # Check if you are already in the environment
+    if [[ $PATH != *"$ENV"* ]]; then
+      source activate $ENV
+      if [ $? -eq 0 ]; then
+        :
+      else
+        echo "Conda env '$ENV' doesn't exist."
+      fi
     fi
   fi
 }
